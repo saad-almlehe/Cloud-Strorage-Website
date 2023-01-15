7dc3ccec8854d30c4d809e67d54ad1066107ed4f
Saad Alsubaie <85945978+saad-almlehe@users.noreply.github.com>
2023-01-15 09:11:41 +0300

    authorization management

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/security.java
@@ -0,0 +1,38 @@
+package com.udacity.jwdnd.course1.cloudstorage.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
+
+@Configuration
+@EnableWebSecurity
+public class security extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationService authenticationService;
+
+    public security(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth){
+        auth.authenticationProvider(this.authenticationService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+        http.authorizeRequests().antMatchers("/signup","/css/**","/js/**")
+                .permitAll().anyRequest().authenticated();
+        ///signup
+        //login
+
+        http.formLogin().loginPage("/login").permitAll();
+
+
+        http.formLogin().defaultSuccessUrl("/home",true);
+    }
+}
